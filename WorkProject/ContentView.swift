2a723cc9725746f9000812bbba7e5523e62da546
Max <barreiro.fontela.max@gmail.com>
2026-06-16 15:23:34 +0300

    Initial Commit

---
+++ WorkProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkProject
+//
+//  Created by MaxAdmin on 16.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkProject/WorkProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkProjectApp.swift
+//  WorkProject
+//
+//  Created by MaxAdmin on 16.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct WorkProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
